5aad83cfa31be4cadab94b67ba5816f70b5d91f9
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-01-06 14:14:05 +0500

    setting routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
+  const MainLayout = () => {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      element:<MainLayout />,
+      children:[
+        {path:'/',element:<Home/>}
+      ]
+    }
+  ])
 
   return (
-    <h1>Helldjflsdjfdsjfldk</h1>
-  )
+    <div className="App">
+      <RouterProvider router={router}/>
+    </div>
+  );
 }
 
-export default App
+export default App;
